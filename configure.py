19912f13b6c17ca759706b28fcd7a8b248a369c5
Ryan Hunter <robocop@lycos.co.uk>
2022-02-01 19:42:38 +0000

    Updated AMBuild API

--- configure.py
+++ configure.py
@@ -1,14 +1,16 @@
-API_VERSION = '2.1'
+# vim: set sts=2 ts=8 sw=2 tw=99 et:
+API_VERSION = '2.2'
 
 import sys
 try:
-  from ambuild2 import run
-  if not run.HasAPI(API_VERSION):
-    raise Exception()
+    from ambuild2 import run
+    if not run.HasAPI(API_VERSION):
+        raise Exception()
 except:
-  sys.stderr.write('AMBuild {0} must be installed to build this project.\n'.format(API_VERSION))
-  sys.stderr.write('http://www.alliedmods.net/ambuild\n')
-  sys.exit(1)
+    sys.stderr.write('AMBuild {0} must be installed to build this project.\n'.format(API_VERSION))
+    sys.stderr.write('http://www.alliedmods.net/ambuild\n')
+    sys.exit(1)
+
 
 prep = run.PrepareBuild(sourcePath=sys.path[0])
 prep.default_build_folder = 'obj-' + prep.target_platform
